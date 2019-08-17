0195b23771da1e8e8be01936e3931ecbd9b316a7
josueneneve <53630116+josueneneve@users.noreply.github.com>
2019-08-16 21:16:47 -0300

    Conversão de Celsius para Fahrenheit

---
+++ CelsiusFahrenheit.php
@@ -0,0 +1,10 @@
+<?php
+
+
+echo ' Convertendo temperatura Celsius em Fahrenheit ';
+
+$celsius = readline("Informe a Temperatura: ");
+
+$fahre = (9 * $celsius + 160);
+
+echo 'Resultado: '.$fahre;
